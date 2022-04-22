d821f3ed7372392decc1b83baeb235dc10b9fd9a
Anup Kashyap <anupkashyap7@gmail.com>
2022-04-21 23:20:21 -0700

    Added nearby discovery

--- app/src/main/java/cse535/group35/mobileoffloading/MasterActivity.java
+++ app/src/main/java/cse535/group35/mobileoffloading/MasterActivity.java
@@ -25,6 +25,9 @@ import com.google.android.gms.nearby.connection.ConnectionResolution;
 import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
 import com.google.android.gms.nearby.connection.DiscoveryOptions;
 import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
 import com.google.android.gms.nearby.connection.Strategy;
 
 import java.util.ArrayList;
@@ -64,14 +67,14 @@ public class MasterActivity extends AppCompatActivity implements View.OnClickLis
     private void startDiscovery() {
         Toast.makeText(this, "Starting Discovery", Toast.LENGTH_SHORT).show();
             DiscoveryOptions discoveryOptions =
-                    new DiscoveryOptions.Builder().setStrategy(Strategy.P2P_STAR).build();
+                    new DiscoveryOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build();
             Nearby.getConnectionsClient(getApplicationContext())
                     .startDiscovery("OFFLOADINGSERVICE", new EndpointDiscoveryCallback() {
                         @Override
-                        public void onEndpointFound(@NonNull String s, @NonNull DiscoveredEndpointInfo discoveredEndpointInfo) {
+                        public void onEndpointFound(@NonNull String endPointId, @NonNull DiscoveredEndpointInfo discoveredEndpointInfo) {
                             Log.d("A", "onEndpointFound: Found device"+ discoveredEndpointInfo.getEndpointName());
                             Toast.makeText(getApplicationContext(), "Found Device: "+discoveredEndpointInfo.getEndpointName(), Toast.LENGTH_SHORT).show();
-                            nearbyDevicesAdapter.add(discoveredEndpointInfo.getEndpointName());
+                            nearbyDevicesAdapter.add(endPointId);
 
                         }
 
@@ -136,39 +139,56 @@ public class MasterActivity extends AppCompatActivity implements View.OnClickLis
     private void initializeDevicesListView() {
         ListView devicesListView = findViewById(devices_listview);
         devicesListView.setAdapter(nearbyDevicesAdapter);
-//        devicesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                 String selected=(String)adapterView.getItemAtPosition(i);
-//                 Toast.makeText(MasterActivity.this, "Selected: " +selected, Toast.LENGTH_SHORT).show();
-//                Nearby.getConnectionsClient(getApplicationContext())
-//                        .requestConnection("MASTER", selected, new ConnectionLifecycleCallback() {
-//                            @Override
-//                            public void onConnectionInitiated(@NonNull String s, @NonNull ConnectionInfo connectionInfo) {
-//
-//                            }
-//
-//                            @Override
-//                            public void onConnectionResult(@NonNull String s, @NonNull ConnectionResolution connectionResolution) {
-//
-//                            }
-//
-//                            @Override
-//                            public void onDisconnected(@NonNull String s) {
-//
-//                            }
-//                        })
-//                        .addOnSuccessListener(
-//                                (Void unused) -> {
-//                                    // We successfully requested a connection. Now both sides
-//                                    // must accept before the connection is established.
-//                                })
-//                        .addOnFailureListener(
-//                                (Exception e) -> {
-//                                    // Nearby Connections failed to request the connection.
-//                                });
-//            }
-//        });
+        devicesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                 String selected=(String)adapterView.getItemAtPosition(i);
+                 Toast.makeText(MasterActivity.this, "Selected: " +selected, Toast.LENGTH_SHORT).show();
+                Nearby.getConnectionsClient(getApplicationContext())
+                        .requestConnection("MASTER", selected, new ConnectionLifecycleCallback() {
+                            @Override
+                            public void onConnectionInitiated(@NonNull String s, @NonNull ConnectionInfo connectionInfo) {
+                                Toast.makeText(MasterActivity.this, "Connection Initiated with "+s, Toast.LENGTH_SHORT).show();
+                                Nearby.getConnectionsClient(getApplicationContext()).acceptConnection(s, new PayloadCallback() {
+                                    @Override
+                                    public void onPayloadReceived(@NonNull String s, @NonNull Payload payload) {
+
+                                    }
+
+                                    @Override
+                                    public void onPayloadTransferUpdate(@NonNull String s, @NonNull PayloadTransferUpdate payloadTransferUpdate) {
+
+                                    }
+                                });
+
+                            }
+
+                            @Override
+                            public void onConnectionResult(@NonNull String s, @NonNull ConnectionResolution connectionResolution) {
+                                String inputString = "Hello World!";
+                                byte[] byteArrray = inputString.getBytes();
+                                Payload bytesPayload = Payload.fromBytes(byteArrray);
+                                Nearby.getConnectionsClient(getApplicationContext()).sendPayload(s, bytesPayload);
+                                Toast.makeText(MasterActivity.this, "Payload sent", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override
+                            public void onDisconnected(@NonNull String s) {
+                                Toast.makeText(MasterActivity.this, "Disconnected", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnSuccessListener(
+                                (Void unused) -> {
+                                    // We successfully requested a connection. Now both sides
+                                    // must accept before the connection is established.
+                                })
+                        .addOnFailureListener(
+                                (Exception e) -> {
+                                    // Nearby Connections failed to request the connection.
+                                });
+            }
+        });
     }
 
     private boolean areAllPermissionsGranted() {
--- app/src/main/java/cse535/group35/mobileoffloading/SlaveActivity.java
+++ app/src/main/java/cse535/group35/mobileoffloading/SlaveActivity.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextPaint;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.nearby.Nearby;
@@ -31,29 +33,34 @@ public class SlaveActivity extends AppCompatActivity {
     {
         Toast.makeText(this, "Started Advertizing", Toast.LENGTH_SHORT).show();
         AdvertisingOptions advertisingOptions =
-                new AdvertisingOptions.Builder().setStrategy(Strategy.P2P_STAR).build();
+                new AdvertisingOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build();
         Nearby.getConnectionsClient(getApplicationContext())
                 .startAdvertising(
                         "Test Slave", "OFFLOADINGSERVICE", new ConnectionLifecycleCallback() {
                             @Override
                             public void onConnectionInitiated(@NonNull String s, @NonNull ConnectionInfo connectionInfo) {
                                 Toast.makeText(SlaveActivity.this, "Accepting Connection from "+ connectionInfo.getEndpointName(), Toast.LENGTH_SHORT).show();
-                                Nearby.getConnectionsClient(getApplicationContext()).acceptConnection(connectionInfo.getEndpointName(), new PayloadCallback() {
+                                PayloadCallback payloadCallback= new PayloadCallback() {
                                     @Override
                                     public void onPayloadReceived(@NonNull String s, @NonNull Payload payload) {
+                                        Log.d("TAG", "onPayloadReceived: Payload received");
+                                        Toast.makeText(getApplicationContext(), "Received message:", Toast.LENGTH_SHORT).show();
 
+                                        String data = new String(payload.asBytes());
+                                        Toast.makeText(SlaveActivity.this, "Received message: "+data, Toast.LENGTH_SHORT).show();
                                     }
 
                                     @Override
                                     public void onPayloadTransferUpdate(@NonNull String s, @NonNull PayloadTransferUpdate payloadTransferUpdate) {
-
+                                        Toast.makeText(SlaveActivity.this, "Payload transfer", Toast.LENGTH_SHORT).show();
                                     }
-                                });
+                                };
+                                Nearby.getConnectionsClient(getApplicationContext()).acceptConnection(s,payloadCallback );
                             }
 
                             @Override
                             public void onConnectionResult(@NonNull String s, @NonNull ConnectionResolution connectionResolution) {
-
+                                Toast.makeText(SlaveActivity.this, "CONNECTION RESULT", Toast.LENGTH_SHORT).show();
                             }
 
                             @Override
